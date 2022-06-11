2e2c3b87450d4802e910c00c3efbf4fd9184d1db
NiAyarza <102627909+NiAyarza@users.noreply.github.com>
2022-06-11 16:41:23 -0400

    Add files via upload

--- clinicaWeb1/core/urls.py
+++ clinicaWeb1/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, inscripcion, productos, quienessomos
 
 urlpatterns = [
     path('', home,name="home"),
+    path('inscripcion/', inscripcion,name="inscripcion"),
+    path('productos/', productos,name="productos"),
+    path('quienessomos/', quienessomos,name="quienessomos"),
 ]
